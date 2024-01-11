cf16fdc529d2f4ee07d8bd1a91b9e8dab19350ab
Biitopher <biitopher@gmail.com>
2024-01-11 15:21:23 +0000

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Calculates the sum of a list of integers and floats"""
+
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """defines sum of the input list elements"""
+    return sum(mxd_lst)
