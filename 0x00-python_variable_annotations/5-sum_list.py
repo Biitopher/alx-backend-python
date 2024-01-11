88efd9cfc0540812ed384a2801d7e721cfb5adb7
Biitopher <biitopher@gmail.com>
2024-01-11 15:13:18 +0000

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Calculates the sum of a list of floating-point numbers"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """The sum of the input list elements"""
+    return sum(input_list)
