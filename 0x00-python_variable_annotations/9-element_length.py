eeafe159a60e6210f818e1258add50f991e481bb
Biitopher <biitopher@gmail.com>
2024-01-11 15:53:42 +0000

    Task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """returns list of tuples where each tuple contains element"""
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """defines elements length"""
     return [(i, len(i)) for i in lst]
