c0b38eab6d92c4fa407431ec49adb3fd2ac38daf
Biitopher <biitopher@gmail.com>
2024-01-11 14:38:46 +0000

    Task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+"""Adds two floating-point numbers and returns their sum"""
 
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two floating-point numbers and returns their sum."""
+    """defines adding of two floating-point numbers and returns sum."""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Concatenates two strings and returns the resulting string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenating two strings"""
+    return str1 + str2
