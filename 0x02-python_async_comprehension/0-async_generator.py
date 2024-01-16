bf29f45f7c04cef80d3be9fa9775b06d2ab13c46
Biitopher <biitopher@gmail.com>
2024-01-16 19:01:18 +0000

    Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,4 +11,4 @@ async def async_generator() -> Generator[float, None, None]:
     """defines async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
