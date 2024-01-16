fdaaec36e8d911b2591924268d32f517c94da52b
Biitopher <biitopher@gmail.com>
2024-01-16 17:41:40 +0000

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """coroutine collectrandom numbers using async and return random numbers"""
 
+
 import asyncio
 import random
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """coroutine random numbers"""
+    async_generator = __import__('0-async_generator').async_generator
     return [x async for x in async_generator()]
