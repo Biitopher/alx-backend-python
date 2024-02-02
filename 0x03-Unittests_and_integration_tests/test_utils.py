5faa41b73b7231fd0838610bfc2ef235bf50a14c
Biitopher <biitopher@gmail.com>
2024-02-02 15:23:16 +0000

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +28,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Checks specified exception is raised fot given inputs"""
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Call the get_json function"""
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_requests_get):
+        """Define test inputs"""
+        test_url = "https://example.com/api/data"
+        test_payload = {"key": "value"}
+
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_requests_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_requests_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
